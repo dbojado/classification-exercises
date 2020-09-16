e28d96d2d80ff47bd2f1e1497f1ee358e6fe57a5
Daniella Bojado <daniella.bojado@gmail.com>
2020-09-16 10:15:38 -0500

    modified python file

--- acquire.py
+++ acquire.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 import os
-from env import host, user, password
+from env import host, username, password
 
 ###################### Acquire Titanic Data ######################
 
-def get_connection(db, user=user, host=host, password=password):
+def get_connection(db, user=username, host=host, password=password):
     '''
     This function uses my info from my env file to
     create a connection url to access the Codeup db.
     '''
-    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+    return f'mysql+pymysql://{username}:{password}@{host}/{db}'
     
 def new_titanic_data():
     '''
